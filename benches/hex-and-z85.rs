c6e7583b0f1e63d4b4e2474a8a08d846ecea65b9
Meadowsys <blazeykirin@gmail.com>
2024-04-04 16:42:18 -0700

    redid the benchmark lol

--- benches/hex-and-z85.rs
+++ benches/hex-and-z85.rs
@@ -9,45 +9,73 @@ fn benchmark(c: &mut Criterion) {
 	let mut rng = thread_rng();
 	let mut bytes = vec![0u8; FIFTY_MIB];
 	rng.fill(&mut *bytes);
-	let bytes = &*bytes;
-
-	let encoded_hex = ::hex::encode(bytes);
-	let encoded_hex = encoded_hex.as_bytes();
-
-	let wiwi_encoded_hex = ::wiwi::hex::encode_hex(bytes);
-	let wiwi_encoded_hex = wiwi_encoded_hex.as_bytes();
+	let bytes = black_box(&*bytes);
 
 	let encoded_z85 = ::z85::encode(bytes);
-	let encoded_z85 = encoded_z85.as_bytes();
+	let encoded_z85 = black_box(encoded_z85.as_bytes());
+	let encoded_z85_wiwi = ::wiwi::z85::encode_z85(bytes);
+	let encoded_z85_wiwi = black_box(encoded_z85_wiwi.as_bytes());
 
-	let wiwi_encoded_z85 = ::wiwi::z85::encode_z85(bytes);
-	let wiwi_encoded_z85 = wiwi_encoded_z85.as_bytes();
+	let encoded_hex = ::hex::encode(bytes);
+	let encoded_hex = black_box(encoded_hex.as_bytes());
+	let encoded_hex_wiwi = ::wiwi::hex::encode_hex(bytes);
+	let encoded_hex_wiwi = black_box(encoded_hex_wiwi.as_bytes());
 
 	c
-		.bench_function("hex::decode 50MiB", |b| b.iter(|| {
-			let _: Vec<u8> = ::hex::decode(black_box(encoded_hex)).unwrap();
+		.bench_function("(50MB) z85 encode", |b| b.iter(|| {
+			let _: String = ::z85::encode(bytes);
 		}))
-		.bench_function("hex::encode 50MiB", |b| b.iter(|| {
-			let _: String = ::hex::encode(black_box(bytes));
+		.bench_function("(50MB) z85 decode", |b| b.iter(|| {
+			let _: Vec<u8> = ::z85::decode(encoded_z85).unwrap();
 		}))
-		.bench_function("wiwi::hex::decode 50MiB", |b| b.iter(|| {
-			let _: Vec<u8> = ::wiwi::hex::decode_hex(black_box(wiwi_encoded_hex)).unwrap();
+		.bench_function("(50MB) z85 encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::z85::encode_z85(bytes);
 		}))
-		.bench_function("wiwi::hex::encode 50MiB", |b| b.iter(|| {
-			let _: String = ::wiwi::hex::encode_hex(black_box(bytes));
+		.bench_function("(50MB) z85 decode (wiwi)", |b| b.iter(|| {
+			let _: Vec<u8> = ::wiwi::z85::decode_z85(encoded_z85_wiwi).unwrap();
 		}))
-		.bench_function("z85::decode 50MiB", |b| b.iter(|| {
-			let _: Vec<u8> = ::z85::decode(black_box(encoded_z85)).unwrap();
+
+		.bench_function("(50MB) hex encode", |b| b.iter(|| {
+			let _: String = ::hex::encode(bytes);
 		}))
-		.bench_function("z85::encode 50MiB", |b| b.iter(|| {
-			let _: String = ::z85::encode(black_box(bytes));
+		.bench_function("(50MB) hex decode", |b| b.iter(|| {
+			let _: Vec<u8> = ::hex::decode(encoded_hex).unwrap();
 		}))
-		.bench_function("wiwi::z85::decode_z85 50MiB", |b| b.iter(|| {
-			let _: Vec<u8> = ::wiwi::z85::decode_z85(black_box(wiwi_encoded_z85)).unwrap();
+		.bench_function("(50MB) hex encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::hex::encode_hex(bytes);
 		}))
-		.bench_function("wiwi::z85::encode_z85 50MiB", |b| b.iter(|| {
-			let _: String = ::wiwi::z85::encode_z85(black_box(bytes));
-		}));
+		.bench_function("(50MB) hex decode (wiwi)", |b| b.iter(|| {
+			let _: Vec<u8> = ::wiwi::hex::decode_hex(encoded_hex_wiwi).unwrap();
+		}))
+
+		// .bench_function("(50MB) base32 encode", |b| b.iter(|| {}))
+		// .bench_function("(50MB) base32 decode", |b| b.iter(|| {}))
+		.bench_function("(50MB) base32 encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::base32::encode_base32(bytes);
+		}))
+		// .bench_function("(50MB) base32 decode (wiwi)", |b| b.iter(|| {}))
+
+		// .bench_function("(50MB) base32hex encode", |b| b.iter(|| {}))
+		// .bench_function("(50MB) base32hex decode", |b| b.iter(|| {}))
+		.bench_function("(50MB) base32hex encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::base32::encode_base32hex(bytes);
+		}))
+		// .bench_function("(50MB) base32hex decode (wiwi)", |b| b.iter(|| {}))
+
+		// .bench_function("(50MB) base64 encode", |b| b.iter(|| {}))
+		// .bench_function("(50MB) base64 decode", |b| b.iter(|| {}))
+		.bench_function("(50MB) base64 encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::base64::encode_base64(bytes);
+		}))
+		// .bench_function("(50MB) base64 decode (wiwi)", |b| b.iter(|| {}))
+
+		// .bench_function("(50MB) base64url encode", |b| b.iter(|| {}))
+		// .bench_function("(50MB) base64url decode", |b| b.iter(|| {}))
+		.bench_function("(50MB) base64url encode (wiwi)", |b| b.iter(|| {
+			let _: String = ::wiwi::base64::encode_base64url(bytes);
+		}))
+		// .bench_function("(50MB) base64url decode (wiwi)", |b| b.iter(|| {}))
+	;
 }
 
 criterion_group!(benches, benchmark);
