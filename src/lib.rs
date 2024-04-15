0f72a1eefc4d6215f46f1fde44ea9e808839ce17
Meadowsys <blazeykirin@gmail.com>
2024-04-15 08:44:15 -0700

    add #[allow(unused_mut)] too because omg it screams

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,12 @@
 #![allow(clippy::new_without_default)]
 
 // TODO: remove when more finished
-#![allow(dead_code, unused_imports, unused_variables)]
+#![allow(
+	dead_code,
+	unused_imports,
+	unused_mut,
+	unused_variables
+)]
 
 use ::cfg_if::cfg_if;
 
