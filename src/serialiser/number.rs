92efa0697b657a48e0eb551d2018dcd04eebe1c2
Meadowsys <blazeykirin@gmail.com>
2024-04-16 23:41:28 -0700

    fixed signed ints

--- src/serialiser/number.rs
+++ src/serialiser/number.rs
@@ -4,24 +4,24 @@ use ::std::convert::identity;
 use ::std::mem::{ MaybeUninit, transmute };
 use ::std::{ ptr, slice };
 
-const LARGEST_8: u16 = u8::MAX as u16;
+const LARGEST_U8: u16 = u8::MAX as u16;
 
-const LARGEST_16: u32 = u16::MAX as u32;
-const LARGEST_24: u32 = LARGEST_16 << 8 | u8::MAX as u32;
+const LARGEST_U16: u32 = u16::MAX as u32;
+const LARGEST_U24: u32 = LARGEST_U16 << 8 | u8::MAX as u32;
 
-const LARGEST_32: u64 = u32::MAX as u64;
-const LARGEST_40: u64 = LARGEST_32 << 8 | u8::MAX as u64;
-const LARGEST_48: u64 = LARGEST_40 << 8 | u8::MAX as u64;
-const LARGEST_56: u64 = LARGEST_48 << 8 | u8::MAX as u64;
+const LARGEST_U32: u64 = u32::MAX as u64;
+const LARGEST_U40: u64 = LARGEST_U32 << 8 | u8::MAX as u64;
+const LARGEST_U48: u64 = LARGEST_U40 << 8 | u8::MAX as u64;
+const LARGEST_U56: u64 = LARGEST_U48 << 8 | u8::MAX as u64;
 
-const LARGEST_64: u128 = u64::MAX as u128;
-const LARGEST_72: u128 = LARGEST_64 << 8 | u8::MAX as u128;
-const LARGEST_80: u128 = LARGEST_72 << 8 | u8::MAX as u128;
-const LARGEST_88: u128 = LARGEST_80 << 8 | u8::MAX as u128;
-const LARGEST_96: u128 = LARGEST_88 << 8 | u8::MAX as u128;
-const LARGEST_104: u128 = LARGEST_96 << 8 | u8::MAX as u128;
-const LARGEST_112: u128 = LARGEST_104 << 8 | u8::MAX as u128;
-const LARGEST_120: u128 = LARGEST_112 << 8 | u8::MAX as u128;
+const LARGEST_U64: u128 = u64::MAX as u128;
+const LARGEST_U72: u128 = LARGEST_U64 << 8 | u8::MAX as u128;
+const LARGEST_U80: u128 = LARGEST_U72 << 8 | u8::MAX as u128;
+const LARGEST_U88: u128 = LARGEST_U80 << 8 | u8::MAX as u128;
+const LARGEST_U96: u128 = LARGEST_U88 << 8 | u8::MAX as u128;
+const LARGEST_U104: u128 = LARGEST_U96 << 8 | u8::MAX as u128;
+const LARGEST_U112: u128 = LARGEST_U104 << 8 | u8::MAX as u128;
+const LARGEST_U120: u128 = LARGEST_U112 << 8 | u8::MAX as u128;
 
 #[inline]
 fn min_marker_u8(num: u8) -> u8 {
@@ -30,42 +30,77 @@ fn min_marker_u8(num: u8) -> u8 {
 
 fn min_marker_u16(num: u16) -> u8 {
 	match num {
-		num @ ..=LARGEST_8 => { min_marker_u8(num as u8) }
+		num @ ..=LARGEST_U8 => { min_marker_u8(num as u8) }
 		num => MARKER_U16
 	}
 }
 
 fn min_marker_u32(num: u32) -> u8 {
 	match num {
-		num @ ..=LARGEST_16 => { min_marker_u16(num as u16) }
-		num @ ..=LARGEST_24 => { MARKER_U24 }
+		num @ ..=LARGEST_U16 => { min_marker_u16(num as u16) }
+		num @ ..=LARGEST_U24 => { MARKER_U24 }
 		num => { MARKER_U32 }
 	}
 }
 
 fn min_marker_u64(num: u64) -> u8 {
 	match num {
-		num @ ..=LARGEST_32 => { min_marker_u32(num as u32) }
-		num @ ..=LARGEST_40 => { MARKER_U40 }
-		num @ ..=LARGEST_48 => { MARKER_U48 }
-		num @ ..=LARGEST_56 => { MARKER_U56 }
+		num @ ..=LARGEST_U32 => { min_marker_u32(num as u32) }
+		num @ ..=LARGEST_U40 => { MARKER_U40 }
+		num @ ..=LARGEST_U48 => { MARKER_U48 }
+		num @ ..=LARGEST_U56 => { MARKER_U56 }
 		num => { MARKER_U64 }
 	}
 }
 
 fn min_marker_u128(num: u128) -> u8 {
 	match num {
-		num @ ..=LARGEST_64 => { min_marker_u64(num as u64) }
-		num @ ..=LARGEST_72 => { MARKER_U72 }
-		num @ ..=LARGEST_80 => { MARKER_U80 }
-		num @ ..=LARGEST_88 => { MARKER_U88 }
-		num @ ..=LARGEST_96 => { MARKER_U96 }
-		num @ ..=LARGEST_104 => { MARKER_U104 }
-		num @ ..=LARGEST_112 => { MARKER_U112 }
-		num @ ..=LARGEST_120 => { MARKER_U120 }
+		num @ ..=LARGEST_U64 => { min_marker_u64(num as u64) }
+		num @ ..=LARGEST_U72 => { MARKER_U72 }
+		num @ ..=LARGEST_U80 => { MARKER_U80 }
+		num @ ..=LARGEST_U88 => { MARKER_U88 }
+		num @ ..=LARGEST_U96 => { MARKER_U96 }
+		num @ ..=LARGEST_U104 => { MARKER_U104 }
+		num @ ..=LARGEST_U112 => { MARKER_U112 }
+		num @ ..=LARGEST_U120 => { MARKER_U120 }
 		num => { MARKER_U128 }
 	}
 }
+
+const LARGEST_I8: i16 = i8::MAX as i16;
+const SMALLEST_I8: i16 = !LARGEST_I8;
+
+const LARGEST_I16: i32 = i16::MAX as i32;
+const SMALLEST_I16: i32 = !LARGEST_I16;
+const LARGEST_I24: i32 = LARGEST_I16 << 8 | u8::MAX as i32;
+const SMALLEST_I24: i32 = !LARGEST_I24;
+
+const LARGEST_I32: i64 = i32::MAX as i64;
+const SMALLEST_I32: i64 = !LARGEST_I32;
+const LARGEST_I40: i64 = LARGEST_I32 << 8 | u8::MAX as i64;
+const SMALLEST_I40: i64 = !LARGEST_I40;
+const LARGEST_I48: i64 = LARGEST_I40 << 8 | u8::MAX as i64;
+const SMALLEST_I48: i64 = !LARGEST_I48;
+const LARGEST_I56: i64 = LARGEST_I48 << 8 | u8::MAX as i64;
+const SMALLEST_I56: i64 = !LARGEST_I56;
+
+const LARGEST_I64: i128 = i64::MAX as i128;
+const SMALLEST_I64: i128 = !LARGEST_I64;
+const LARGEST_I72: i128 = LARGEST_I64 << 8 | u8::MAX as i128;
+const SMALLEST_I72: i128 = !LARGEST_I72;
+const LARGEST_I80: i128 = LARGEST_I72 << 8 | u8::MAX as i128;
+const SMALLEST_I80: i128 = !LARGEST_I80;
+const LARGEST_I88: i128 = LARGEST_I80 << 8 | u8::MAX as i128;
+const SMALLEST_I88: i128 = !LARGEST_I88;
+const LARGEST_I96: i128 = LARGEST_I88 << 8 | u8::MAX as i128;
+const SMALLEST_I96: i128 = !LARGEST_I96;
+const LARGEST_I104: i128 = LARGEST_I96 << 8 | u8::MAX as i128;
+const SMALLEST_I104: i128 = !LARGEST_I104;
+const LARGEST_I112: i128 = LARGEST_I104 << 8 | u8::MAX as i128;
+const SMALLEST_I112: i128 = !LARGEST_I112;
+const LARGEST_I120: i128 = LARGEST_I112 << 8 | u8::MAX as i128;
+const SMALLEST_I120: i128 = !LARGEST_I120;
+
 
 #[inline]
 fn min_marker_i8(num: i8) -> u8 {
@@ -73,40 +108,40 @@ fn min_marker_i8(num: i8) -> u8 {
 }
 
 fn min_marker_i16(num: i16) -> u8 {
-	match unsafe { transmute(num) } {
-		num @ ..=LARGEST_8 => unsafe { min_marker_i8(transmute(num as u8)) }
+	match num {
+		num @ SMALLEST_I8..=LARGEST_I8 => { min_marker_i8(num as i8) }
 		num => { MARKER_I16 }
 	}
 }
 
 fn min_marker_i32(num: i32) -> u8 {
-	match unsafe { transmute(num) } {
-		num @ ..=LARGEST_16 => unsafe { min_marker_i16(transmute(num as u16)) }
-		num @ ..=LARGEST_24 => { MARKER_I24 }
+	match num {
+		num @ SMALLEST_I16..=LARGEST_I16 => { min_marker_i16(num as i16) }
+		num @ SMALLEST_I24..=LARGEST_I24 => { MARKER_I24 }
 		num => { MARKER_I32 }
 	}
 }
 
 fn min_marker_i64(num: i64) -> u8 {
-	match unsafe { transmute(num) } {
-		num @ ..=LARGEST_32 => unsafe { min_marker_i32(transmute(num as u32)) }
-		num @ ..=LARGEST_40 => { MARKER_I40 }
-		num @ ..=LARGEST_48 => { MARKER_I48 }
-		num @ ..=LARGEST_56 => { MARKER_I56 }
+	match num {
+		num @ SMALLEST_I32..=LARGEST_I32 => { min_marker_i32(num as i32) }
+		num @ SMALLEST_I40..=LARGEST_I40 => { MARKER_I40 }
+		num @ SMALLEST_I48..=LARGEST_I48 => { MARKER_I48 }
+		num @ SMALLEST_I56..=LARGEST_I56 => { MARKER_I56 }
 		num => { MARKER_I64 }
 	}
 }
 
 fn min_marker_i128(num: i128) -> u8 {
-	match unsafe { transmute(num) } {
-		num @ ..=LARGEST_64 => unsafe { min_marker_i64(transmute(num as u64)) }
-		num @ ..=LARGEST_72 => { MARKER_I72 }
-		num @ ..=LARGEST_80 => { MARKER_I80 }
-		num @ ..=LARGEST_88 => { MARKER_I88 }
-		num @ ..=LARGEST_96 => { MARKER_I96 }
-		num @ ..=LARGEST_104 => { MARKER_I104 }
-		num @ ..=LARGEST_112 => { MARKER_I112 }
-		num @ ..=LARGEST_120 => { MARKER_I120 }
+	match num {
+		num @ SMALLEST_I64..=LARGEST_I64 => { min_marker_i64(num as i64) }
+		num @ SMALLEST_I72..=LARGEST_I72 => { MARKER_I72 }
+		num @ SMALLEST_I80..=LARGEST_I80 => { MARKER_I80 }
+		num @ SMALLEST_I88..=LARGEST_I88 => { MARKER_I88 }
+		num @ SMALLEST_I96..=LARGEST_I96 => { MARKER_I96 }
+		num @ SMALLEST_I104..=LARGEST_I104 => { MARKER_I104 }
+		num @ SMALLEST_I112..=LARGEST_I112 => { MARKER_I112 }
+		num @ SMALLEST_I120..=LARGEST_I120 => { MARKER_I120 }
 		num => { MARKER_I128 }
 	}
 }
@@ -236,14 +271,14 @@ fn marker_is_valid_i128(marker: u8) -> bool {
 	}
 }
 macro_rules! impl_number_deserialise {
-	($($num:ty: $marker_fn:ident)*) => {
+	($($num:ty: $marker_fn:ident, $init_byte:expr)*) => {
 		$(
 			impl<'h> Deserialise<'h> for $num {
 				fn deserialise<B: BufferImplRead>(input: &mut B) -> Result<Self> {
 					let marker = input.read_next_byte().err_eof()?;
 
 					if $marker_fn(marker) {
-						let mut buf = [0u8; { <$num>::BITS as usize / 8 }];
+						let mut buf = [$init_byte; { <$num>::BITS as usize / 8 }];
 						let count = (marker >> 1) as usize;
 
 						unsafe {
@@ -268,15 +303,15 @@ macro_rules! impl_number_deserialise {
 }
 
 impl_number_deserialise! {
-	u8: marker_is_valid_u8
-	u16: marker_is_valid_u16
-	u32: marker_is_valid_u32
-	u64: marker_is_valid_u64
-	u128: marker_is_valid_u128
+	u8: marker_is_valid_u8, 0
+	u16: marker_is_valid_u16, 0
+	u32: marker_is_valid_u32, 0
+	u64: marker_is_valid_u64, 0
+	u128: marker_is_valid_u128, 0
 
-	i8: marker_is_valid_i8
-	i16: marker_is_valid_i16
-	i32: marker_is_valid_i32
-	i64: marker_is_valid_i64
-	i128: marker_is_valid_i128
+	i8: marker_is_valid_i8, !0
+	i16: marker_is_valid_i16, !0
+	i32: marker_is_valid_i32, !0
+	i64: marker_is_valid_i64, !0
+	i128: marker_is_valid_i128, !0
 }
