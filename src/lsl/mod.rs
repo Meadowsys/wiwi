327aa87997051b53074bf2d2a1dd0da33f2f877a
Meadowsys <blazeykirin@gmail.com>
2024-05-06 11:48:15 -0700

    had some time to think lol

--- src/lsl/mod.rs
+++ src/lsl/mod.rs
@@ -1,15 +1,15 @@
 use std::marker::PhantomData;
 
-// mod consts;
-mod ctx;
-mod render;
-mod ty;
-mod util;
-mod val;
-mod var;
+// // mod consts;
+// mod ctx;
+// mod render;
+// mod ty;
+// mod util;
+// mod val;
+// mod var;
 
-pub use ctx::script::*;
-pub use ctx::state::*;
-pub use var::*;
+// pub use ctx::script::*;
+// pub use ctx::state::*;
+// pub use var::*;
 
 type SingleThreadMarker = PhantomData<*const ()>;
