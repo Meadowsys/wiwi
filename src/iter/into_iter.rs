82a58210aef32f30139349f8565977ed032fe3fa
Meadowsys <blazeykirin@gmail.com>
2024-05-17 14:33:16 -0700

    fumbled around a lot, trying to find a good design
    ... especially one that doesn't make rustc screm at me "aaaaaaaaa conflicting traits"

---
+++ src/iter/into_iter.rs
@@ -0,0 +1,18 @@
+use super::Iter;
+
+pub trait IntoIter {
+	type Item;
+	type Iter: Iter<Item = Self::Item>;
+
+	fn into_wiwi_iter(self) -> Self::Iter;
+}
+
+impl<I: Iter> IntoIter for I {
+	type Item = I::Item;
+	type Iter = Self;
+
+	#[inline]
+	fn into_wiwi_iter(self) -> Self {
+		self
+	}
+}
--- src/iter/mod.rs
+++ src/iter/mod.rs
@@ -1,5 +1,106 @@
-mod adapter;
-mod traits;
+mod into_iter;
 
-pub use adapter::{ AdaptStd, StdIterAdapter };
-pub use traits::Iter;
+pub trait Iter {
+	type Item;
+
+	fn next(&mut self) -> Option<Self::Item>;
+
+	/*
+	fn as_std_iter
+	next_chunk
+	size_hint
+	count
+	last
+	advance_by
+	nth
+	step_by
+	chain
+	zip
+	intersperse
+	intersperse_with
+	map
+	for_each
+	filter
+	filter_map
+	enumerate
+	peekable
+	skip_while
+	take_while
+	map_while
+	skip
+	take
+	scan
+	flat_map
+	flatten
+	map_windows
+	fuse
+	inspect
+	by_ref
+	collect
+	try_collect
+	collect_into
+	partition
+	partition_in_place
+	is_partitioned
+	try_fold
+	try_for_each
+	fold
+	reduce
+	try_reduce
+	all
+	any
+	find
+	find_map
+	try_find
+	position
+	rposition
+	max
+	min
+	max_by_key
+	max_by
+	min_by_key
+	min_by
+	rev
+	unzip
+	copied
+	cloned
+	cycle
+	array_chunks
+	sum
+	product
+	cmp
+	cmp_by
+	partial_cmp
+	partial_cmp_by
+	eq
+	eq_by
+	ne
+	lt
+	le
+	gt
+	ge
+	is_sorted/by/key
+
+	traits
+	product, sum
+	extend, from/intoiter
+	double ended iter
+	exact size iter
+	iter
+	fused iter, trusted len
+	in place iterable
+	trusted fused/step
+	unchecked iter
+	trusted/step
+
+	fns
+	empty
+	from_fn
+	once/with
+	repeat/with
+	successors
+	zip
+	from_coroutine
+	repeat_n
+	*/
+}
