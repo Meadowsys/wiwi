4b3e6594dd4ec5a3a143cc4cec0c82dd88284ed0
Meadowsys <blazeykirin@gmail.com>
2024-06-05 20:48:13 -0700

    extend h module with H

--- src/h/mod.rs
+++ src/h/mod.rs
@@ -5,10 +5,22 @@ pub fn h() -> String {
 	"h".into()
 }
 
+/// H
+#[allow(non_snake_case)] // shut
+pub fn H() -> String {
+	"H".into()
+}
+
 #[cfg(test)]
 mod tests {
 	#[test]
 	pub fn h() {
 		assert_eq!(super::h(), "h", "h should be h");
+	}
+
+	#[test]
+	#[allow(non_snake_case)] // shut
+	pub fn H() {
+		assert_eq!(super::H(), "H", "H should be H");
 	}
 }
