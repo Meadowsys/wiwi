b710ac01c48171f0a59d80b33da49ddb11c7f06e
Meadowsys <blazeykirin@gmail.com>
2024-06-23 14:48:35 -0700

    hmm... add "bigint" trait, for methods that borrow / modify self / don't rely on copying

--- src/num_traits/mod.rs
+++ src/num_traits/mod.rs
@@ -1,3 +1,45 @@
+pub trait BigInt<const BYTES: usize>: Sized {
+	const MIN: Self;
+	const MAX: Self;
+	const BITS: Self;
+	// TODO: this / generic param ehh weird funny
+	const BYTES: Self;
+
+	fn count_ones(&self) -> Self;
+	fn count_zeros(&self) -> Self;
+	fn leading_zeros(&self) -> Self;
+	fn trailing_zeros(&self) -> Self;
+	fn leading_ones(&self) -> Self;
+	fn trailing_ones(&self) -> Self;
+
+	fn rotate_left(&mut self, n: &Self);
+	fn rotate_right(&mut self, n: &Self);
+
+	fn from_be_ref(x: &Self) -> Self;
+	fn from_le_ref(x: &Self) -> Self;
+	fn from_be(x: Self) -> Self;
+	fn from_le(x: Self) -> Self;
+
+	fn to_be(&self) -> Self;
+	fn to_le(&self) -> Self;
+	fn into_be(self) -> Self;
+	fn into_le(self) -> Self;
+
+	fn from_be_bytes_ref(bytes: &[u8; BYTES]) -> Self;
+	fn from_le_bytes_ref(bytes: &[u8; BYTES]) -> Self;
+	fn from_ne_bytes_ref(bytes: &[u8; BYTES]) -> Self;
+	fn from_be_bytes(bytes: [u8; BYTES]) -> Self;
+	fn from_le_bytes(bytes: [u8; BYTES]) -> Self;
+	fn from_ne_bytes(bytes: [u8; BYTES]) -> Self;
+
+	fn to_be_bytes(&self) -> [u8; BYTES];
+	fn to_le_bytes(&self) -> [u8; BYTES];
+	fn to_ne_bytes(&self) -> [u8; BYTES];
+	fn into_be_bytes(self) -> [u8; BYTES];
+	fn into_le_bytes(self) -> [u8; BYTES];
+	fn into_ne_bytes(self) -> [u8; BYTES];
+}
+
 pub trait Int<const BYTES: usize>: Sized + Copy {
 	const MIN: Self;
 	const MAX: Self;
@@ -25,91 +67,133 @@ pub trait Int<const BYTES: usize>: Sized + Copy {
 	fn to_be_bytes(self) -> [u8; BYTES];
 	fn to_le_bytes(self) -> [u8; BYTES];
 	fn to_ne_bytes(self) -> [u8; BYTES];
-
-	// TODO: swap_bytes
-	// TODO: reverse_bits
-	// TODO: checked_add
-	// TODO: strict_add
-	// TODO: unchecked_add
-	// TODO: checked_add_signed
-	// TODO: strict_add_signed
-	// TODO: checked_sub
-	// TODO: strict_sub
-	// TODO: unchecked_sub
-	// TODO: checked_mul
-	// TODO: strict_mul
-	// TODO: unchecked_mul
-	// TODO: checked_div
-	// TODO: strict_div
-	// TODO: checked_div_euclid
-	// TODO: strict_div_euclid
-	// TODO: checked_rem
-	// TODO: strict_rem
-	// TODO: checked_rem_euclid
-	// TODO: strict_rem_euclid
-	// TODO: ilog
-	// TODO: ilog2
-	// TODO: ilog10
-	// TODO: checked_ilog
-	// TODO: checked_ilog2
-	// TODO: checked_ilog10
-	// TODO: checked_neg
-	// TODO: strict_neg
-	// TODO: checked_shl
-	// TODO: strict_shl
-	// TODO: unchecked_shl
-	// TODO: checked_shr
-	// TODO: strict_shr
-	// TODO: unchecked_shr
-	// TODO: checked_pow
-	// TODO: strict_pow
-	// TODO: saturating_add
-	// TODO: saturating_add_signed
-	// TODO: saturating_sub
-	// TODO: saturating_mul
-	// TODO: saturating_div
-	// TODO: saturating_pow
-	// TODO: wrapping_add
-	// TODO: wrapping_add_signed
-	// TODO: wrapping_sub
-	// TODO: wrapping_mul
-	// TODO: wrapping_div
-	// TODO: wrapping_div_euclid
-	// TODO: wrapping_rem
-	// TODO: wrapping_rem_euclid
-	// TODO: wrapping_neg
-	// TODO: wrapping_shl
-	// TODO: wrapping_shr
-	// TODO: wrapping_pow
-	// TODO: overflowing_add
-	// TODO: carrying_add
-	// TODO: overflowing_add_signed
-	// TODO: overflowing_sub
-	// TODO: borrowing_sub
-	// TODO: abs_diff
-	// TODO: overflowing_mul
-	// TODO: overflowing_div
-	// TODO: overflowing_div_euclid
-	// TODO: overflowing_rem
-	// TODO: overflowing_rem_euclid
-	// TODO: overflowing_neg
-	// TODO: overflowing_shl
-	// TODO: overflowing_shr
-	// TODO: overflowing_pow
-	// TODO: pow
-	// TODO: isqrt
-	// TODO: div_euclid
-	// TODO: rem_euclid
-	// TODO: div_floor
-	// TODO: div_ceil
-	// TODO: next_multiple_of
-	// TODO: checked_next_multiple_of
-	// TODO: is_power_of_two
-	// TODO: next_power_of_two
-	// TODO: checked_next_power_of_two
-	// TODO: wrapping_next_power_of_two
-	// TODO: midpoint
 }
+
+impl<const BYTES: usize, T: Int<BYTES>> BigInt<BYTES> for T {
+	const MIN: Self = <Self as Int<BYTES>>::MIN;
+	const MAX: Self = <Self as Int<BYTES>>::MAX;
+	const BITS: Self = <Self as Int<BYTES>>::BITS;
+	const BYTES: Self = <Self as Int<BYTES>>::BYTES;
+
+	fn count_ones(&self) -> Self { Int::<BYTES>::count_ones(*self) }
+	fn count_zeros(&self) -> Self { Int::<BYTES>::count_zeros(*self) }
+	fn leading_zeros(&self) -> Self { Int::<BYTES>::leading_zeros(*self) }
+	fn trailing_zeros(&self) -> Self { Int::<BYTES>::trailing_zeros(*self) }
+	fn leading_ones(&self) -> Self { Int::<BYTES>::leading_ones(*self) }
+	fn trailing_ones(&self) -> Self { Int::<BYTES>::trailing_ones(*self) }
+
+	fn rotate_left(&mut self, n: &Self) { *self = Int::<BYTES>::rotate_left(*self, *n) }
+	fn rotate_right(&mut self, n: &Self) { *self = Int::<BYTES>::rotate_right(*self, *n) }
+
+	fn from_be_ref(x: &Self) -> Self { Int::<BYTES>::from_be(*x) }
+	fn from_le_ref(x: &Self) -> Self { Int::<BYTES>::from_le(*x) }
+	fn from_be(x: Self) -> Self { Int::<BYTES>::from_be(x) }
+	fn from_le(x: Self) -> Self { Int::<BYTES>::from_le(x) }
+
+	fn to_be(&self) -> Self { Int::<BYTES>::to_be(*self) }
+	fn to_le(&self) -> Self { Int::<BYTES>::to_le(*self) }
+	fn into_be(self) -> Self { Int::<BYTES>::to_be(self) }
+	fn into_le(self) -> Self { Int::<BYTES>::to_le(self) }
+
+	fn from_be_bytes_ref(bytes: &[u8; BYTES]) -> Self { Int::<BYTES>::from_be_bytes(*bytes) }
+	fn from_le_bytes_ref(bytes: &[u8; BYTES]) -> Self { Int::<BYTES>::from_le_bytes(*bytes) }
+	fn from_ne_bytes_ref(bytes: &[u8; BYTES]) -> Self { Int::<BYTES>::from_ne_bytes(*bytes) }
+	fn from_be_bytes(bytes: [u8; BYTES]) -> Self { Int::<BYTES>::from_be_bytes(bytes) }
+	fn from_le_bytes(bytes: [u8; BYTES]) -> Self { Int::<BYTES>::from_le_bytes(bytes) }
+	fn from_ne_bytes(bytes: [u8; BYTES]) -> Self { Int::<BYTES>::from_ne_bytes(bytes) }
+
+	fn to_be_bytes(&self) -> [u8; BYTES] { Int::<BYTES>::to_be_bytes(*self) }
+	fn to_le_bytes(&self) -> [u8; BYTES] { Int::<BYTES>::to_le_bytes(*self) }
+	fn to_ne_bytes(&self) -> [u8; BYTES] { Int::<BYTES>::to_ne_bytes(*self) }
+	fn into_be_bytes(self) -> [u8; BYTES] { Int::<BYTES>::to_be_bytes(self) }
+	fn into_le_bytes(self) -> [u8; BYTES] { Int::<BYTES>::to_le_bytes(self) }
+	fn into_ne_bytes(self) -> [u8; BYTES] { Int::<BYTES>::to_ne_bytes(self) }
+}
+
+// TODO: swap_bytes
+// TODO: reverse_bits
+// TODO: checked_add
+// TODO: strict_add
+// TODO: unchecked_add
+// TODO: checked_add_signed
+// TODO: strict_add_signed
+// TODO: checked_sub
+// TODO: strict_sub
+// TODO: unchecked_sub
+// TODO: checked_mul
+// TODO: strict_mul
+// TODO: unchecked_mul
+// TODO: checked_div
+// TODO: strict_div
+// TODO: checked_div_euclid
+// TODO: strict_div_euclid
+// TODO: checked_rem
+// TODO: strict_rem
+// TODO: checked_rem_euclid
+// TODO: strict_rem_euclid
+// TODO: ilog
+// TODO: ilog2
+// TODO: ilog10
+// TODO: checked_ilog
+// TODO: checked_ilog2
+// TODO: checked_ilog10
+// TODO: checked_neg
+// TODO: strict_neg
+// TODO: checked_shl
+// TODO: strict_shl
+// TODO: unchecked_shl
+// TODO: checked_shr
+// TODO: strict_shr
+// TODO: unchecked_shr
+// TODO: checked_pow
+// TODO: strict_pow
+// TODO: saturating_add
+// TODO: saturating_add_signed
+// TODO: saturating_sub
+// TODO: saturating_mul
+// TODO: saturating_div
+// TODO: saturating_pow
+// TODO: wrapping_add
+// TODO: wrapping_add_signed
+// TODO: wrapping_sub
+// TODO: wrapping_mul
+// TODO: wrapping_div
+// TODO: wrapping_div_euclid
+// TODO: wrapping_rem
+// TODO: wrapping_rem_euclid
+// TODO: wrapping_neg
+// TODO: wrapping_shl
+// TODO: wrapping_shr
+// TODO: wrapping_pow
+// TODO: overflowing_add
+// TODO: carrying_add
+// TODO: overflowing_add_signed
+// TODO: overflowing_sub
+// TODO: borrowing_sub
+// TODO: abs_diff
+// TODO: overflowing_mul
+// TODO: overflowing_div
+// TODO: overflowing_div_euclid
+// TODO: overflowing_rem
+// TODO: overflowing_rem_euclid
+// TODO: overflowing_neg
+// TODO: overflowing_shl
+// TODO: overflowing_shr
+// TODO: overflowing_pow
+// TODO: pow
+// TODO: isqrt
+// TODO: div_euclid
+// TODO: rem_euclid
+// TODO: div_floor
+// TODO: div_ceil
+// TODO: next_multiple_of
+// TODO: checked_next_multiple_of
+// TODO: is_power_of_two
+// TODO: next_power_of_two
+// TODO: checked_next_power_of_two
+// TODO: wrapping_next_power_of_two
+// TODO: midpoint
+
 
 macro_rules! int_trait_impl {
 	{ $($int:ident)* } => {
