c40f2138cbc02efa0b8a3630ab109e6536f563b9
Meadowsys <blazeykirin@gmail.com>
2024-06-26 16:16:54 -0700

    comment out every add operation except overflowing for now
    they would have worked for unsigned ints, but I'm not sure yet how to tackle signed ints, so I leave it for now

--- src/int/add.rs
+++ src/int/add.rs
@@ -1,65 +1,65 @@
 use crate::num_traits::Int;
 use std::mem::MaybeUninit;
 
-/// Performs standard addition, with overflow checking depending on overflow
-/// checks compiler option
-///
-/// NOTE: `cfg(overflow_checks)` is for some reason still unstable, so for now,
-/// without the `nightly` feature, this will panic on overflow depending on
-/// `cfg(debug_assertions)` (which is usually enabled in debug and disabled in release)
-pub fn add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> [I; BYTES] {
-	let (res, overflow) = overflowing_add(int1, int2);
+// /// Performs standard addition, with overflow checking depending on overflow
+// /// checks compiler option
+// ///
+// /// NOTE: `cfg(overflow_checks)` is for some reason still unstable, so for now,
+// /// without the `nightly` feature, this will panic on overflow depending on
+// /// `cfg(debug_assertions)` (which is usually enabled in debug and disabled in release)
+// pub fn add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> [I; BYTES] {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+//
+// 	#[cfg_attr(not(feature = "nightly"), cfg(debug_assertions))]
+// 	#[cfg_attr(feature = "nightly", cfg(overflow_checks))]
+// 	assert!(!overflow, "attempt to add with overflow");
+//
+// 	res
+// }
 
-	#[cfg_attr(not(feature = "nightly"), cfg(debug_assertions))]
-	#[cfg_attr(feature = "nightly", cfg(overflow_checks))]
-	assert!(!overflow, "attempt to add with overflow");
+// pub fn checked_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> Option<[I; BYTES]> {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+// 	if overflow { None } else { Some(res) }
+// }
 
-	res
-}
+// pub fn strict_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> [I; BYTES] {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+// 	assert!(!overflow, "attempt to add with overflow");
+// 	res
+// }
 
-pub fn checked_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> Option<[I; BYTES]> {
-	let (res, overflow) = overflowing_add(int1, int2);
-	if overflow { None } else { Some(res) }
-}
+// pub unsafe fn unchecked_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> [I; BYTES] {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+// 	res
+// }
 
-pub fn strict_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> [I; BYTES] {
-	let (res, overflow) = overflowing_add(int1, int2);
-	assert!(!overflow, "attempt to add with overflow");
-	res
-}
+// pub fn saturating_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> [I; BYTES] {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+// 	if overflow { [I::MAX; BYTES] } else { res }
+// }
 
-pub unsafe fn unchecked_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> [I; BYTES] {
-	let (res, overflow) = overflowing_add(int1, int2);
-	res
-}
-
-pub fn saturating_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> [I; BYTES] {
-	let (res, overflow) = overflowing_add(int1, int2);
-	if overflow { [I::MAX; BYTES] } else { res }
-}
-
-pub fn wrapping_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
-	int1: [I; BYTES],
-	int2: [I; BYTES]
-) -> [I; BYTES] {
-	let (res, overflow) = overflowing_add(int1, int2);
-	res
-}
+// pub fn wrapping_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
+// 	int1: [I; BYTES],
+// 	int2: [I; BYTES]
+// ) -> [I; BYTES] {
+// 	let (res, overflow) = overflowing_add(int1, int2);
+// 	res
+// }
 
 pub fn overflowing_add<I: Int<BYTES_PER_INT>, const BYTES_PER_INT: usize, const BYTES: usize>(
 	int1: [I; BYTES],
