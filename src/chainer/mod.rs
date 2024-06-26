e9c3880d40e8417a787ea9a830e07f9407b96832
Meadowsys <blazeykirin@gmail.com>
2024-06-25 21:45:35 -0700

    impl partialeq/partialord/eq/ord for chainers

--- src/chainer/mod.rs
+++ src/chainer/mod.rs
@@ -21,6 +21,59 @@ macro_rules! chainer {
 
 	(@doclink nonchain: $($nonchain:tt)+) => { stringify!($($nonchain)+) };
 
+	// implements PartialEq and PartialOrd
+	{
+		@gen_partial_cmp
+		[$([$($generics_decl:tt)*])?]
+		[$($nonchain:tt)+]
+
+		[$($left:tt)+]
+		[$left_expr:expr]
+
+		[$($right:tt)+]
+		[$right_expr:expr]
+	} => {
+		impl$(<$($generics_decl)*>)? ::std::cmp::PartialEq<$($right)+> for $($left)+
+		where
+			$($nonchain)+: ::std::cmp::PartialEq<$($nonchain)+>
+		{
+			fn eq(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialEq<$($nonchain)+>>::eq($left_expr(self), $right_expr(other))
+			}
+
+			// overriding this in case nonchain has overridden it
+			#[allow(clippy::partialeq_ne_impl)]
+			fn ne(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialEq<$($nonchain)+>>::ne($left_expr(self), $right_expr(other))
+			}
+		}
+
+		impl$(<$($generics_decl)*>)? ::std::cmp::PartialOrd<$($right)+> for $($left)+
+		where
+			$($nonchain)+: ::std::cmp::PartialOrd<$($nonchain)+>
+		{
+			fn partial_cmp(&self, other: &$($right)+) -> ::std::option::Option<::std::cmp::Ordering> {
+				<$($nonchain)+ as ::std::cmp::PartialOrd<$($nonchain)+>>::partial_cmp($left_expr(self), $right_expr(other))
+			}
+
+			fn lt(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialOrd<$($nonchain)+>>::lt($left_expr(self), $right_expr(other))
+			}
+
+			fn le(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialOrd<$($nonchain)+>>::le($left_expr(self), $right_expr(other))
+			}
+
+			fn gt(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialOrd<$($nonchain)+>>::gt($left_expr(self), $right_expr(other))
+			}
+
+			fn ge(&self, other: &$($right)+) -> bool {
+				<$($nonchain)+ as ::std::cmp::PartialOrd<$($nonchain)+>>::ge($left_expr(self), $right_expr(other))
+			}
+		}
+	};
+
 	{
 		$(#[$meta:meta])*
 		$(doclink: $doclink:literal)?
@@ -55,6 +108,7 @@ macro_rules! chainer {
 			type NonChain = $($nonchain)+;
 		}
 
+		// nonchain -> chain conversion
 		impl$(<$($generics_decl)*>)? ::std::convert::From<$($nonchain)+> for $chainer$(<$($generics)*>)? {
 			#[inline(always)]
 			fn from(nonchain: $($nonchain)+) -> Self {
@@ -62,6 +116,7 @@ macro_rules! chainer {
 			}
 		}
 
+		// chain -> nonchain conversion
 		impl$(<$($generics_decl)*>)? ::std::convert::From<$chainer$(<$($generics)*>)?> for $($nonchain)+ {
 			#[inline(always)]
 			fn from(chainer: $chainer$(<$($generics)*>)?) -> Self {
@@ -69,6 +124,7 @@ macro_rules! chainer {
 			}
 		}
 
+		// borrow nonchain mutably
 		impl$(<$($generics_decl)*>)? ::std::convert::AsMut<$($nonchain)+> for $chainer$(<$($generics)*>)? {
 			#[inline(always)]
 			fn as_mut(&mut self) -> &mut $($nonchain)+ {
@@ -76,6 +132,7 @@ macro_rules! chainer {
 			}
 		}
 
+		// borrow nonchain immutably
 		impl$(<$($generics_decl)*>)? ::std::convert::AsRef<$($nonchain)+> for $chainer$(<$($generics)*>)? {
 			#[inline(always)]
 			fn as_ref(&self) -> &$($nonchain)+ {
@@ -109,6 +166,7 @@ macro_rules! chainer {
 			}
 		}
 
+		// default, using inner's default impl
 		impl$(<$($generics_decl)*>)? ::std::default::Default for $chainer$(<$($generics)*>)?
 		where
 			$($nonchain)+: ::std::default::Default
@@ -118,6 +176,7 @@ macro_rules! chainer {
 			}
 		}
 
+		// forwards display to inner, so chain wrapper does not get in the way of it at all
 		impl$(<$($generics_decl)*>)? ::std::fmt::Display for $chainer$(<$($generics)*>)?
 		where
 			$($nonchain)+: ::std::fmt::Display
@@ -125,6 +184,71 @@ macro_rules! chainer {
 			fn fmt(&self, f: &mut ::std::fmt::Formatter<'_>) -> ::std::fmt::Result {
 				::std::fmt::Display::fmt(self.as_nonchain(), f)
 			}
+		}
+
+		impl$(<$($generics_decl)*>)? ::std::cmp::Eq for $chainer$(<$($generics)*>)?
+		where
+			$($nonchain)+: ::std::cmp::Eq
+		{}
+
+		impl$(<$($generics_decl)*>)? ::std::cmp::Ord for $chainer$(<$($generics)*>)?
+		where
+			$($nonchain)+: ::std::cmp::Ord
+		{
+			fn cmp(&self, other: &Self) -> ::std::cmp::Ordering {
+				<$($nonchain)+ as ::std::cmp::Ord>::cmp(self.as_nonchain(), other.as_nonchain())
+			}
+
+			fn max(self, other: Self) -> Self {
+				<$($nonchain)+ as ::std::cmp::Ord>::max(self.into_nonchain(), other.into_nonchain()).into()
+			}
+
+			fn min(self, other: Self) -> Self {
+				<$($nonchain)+ as ::std::cmp::Ord>::min(self.into_nonchain(), other.into_nonchain()).into()
+			}
+
+			fn clamp(self, min: Self, max: Self) -> Self {
+				<$($nonchain)+ as ::std::cmp::Ord>::clamp(self.into_nonchain(), min.into_nonchain(), max.into_nonchain()).into()
+			}
+		}
+
+		// chain / chain
+		$crate::chainer::chainer! {
+			@gen_partial_cmp
+			[$([$($generics_decl)*])?]
+			[$($nonchain)+]
+
+			[$chainer$(<$($generics)*>)?]
+			[$crate::chainer::traits::ChainHalf::as_nonchain]
+
+			[$chainer$(<$($generics)*>)?]
+			[$crate::chainer::traits::ChainHalf::as_nonchain]
+		}
+
+		// chain / nonchain
+		$crate::chainer::chainer! {
+			@gen_partial_cmp
+			[$([$($generics_decl)*])?]
+			[$($nonchain)+]
+
+			[$($nonchain)+]
+			[::std::convert::identity]
+
+			[$chainer$(<$($generics)*>)?]
+			[$crate::chainer::traits::ChainHalf::as_nonchain]
+		}
+
+		// nonchain / chain
+		$crate::chainer::chainer! {
+			@gen_partial_cmp
+			[$([$($generics_decl)*])?]
+			[$($nonchain)+]
+
+			[$chainer$(<$($generics)*>)?]
+			[$crate::chainer::traits::ChainHalf::as_nonchain]
+
+			[$($nonchain)+]
+			[::std::convert::identity]
 		}
 	};
 }
