42dfe6971c9a05e5778da41222430f5189ac1e31
Meadowsys <blazeykirin@gmail.com>
2024-06-26 20:17:43 -0700

    add array conversion trait

--- src/num_traits/mod.rs
+++ src/num_traits/mod.rs
@@ -368,3 +368,13 @@ pub trait XorRegular: Sized + Base + ops::BitXor<Self, Output = Self> {
 }
 
 // TODO: ilog/2/10 sum(?) product(?)
+
+pub trait ArrayConversions<const BYTES: usize>: Sized + Base {
+	fn into_le_bytes(self) -> [u8; BYTES];
+	fn into_be_bytes(self) -> [u8; BYTES];
+	fn into_ne_bytes(self) -> [u8; BYTES];
+
+	fn from_le_bytes(bytes: [u8; BYTES]) -> Self;
+	fn from_be_bytes(bytes: [u8; BYTES]) -> Self;
+	fn from_ne_bytes(bytes: [u8; BYTES]) -> Self;
+}
