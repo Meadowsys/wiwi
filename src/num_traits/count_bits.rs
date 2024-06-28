fa3947b0a470565889e9ffefa6c2d1c564484f61
Meadowsys <blazeykirin@gmail.com>
2024-06-28 14:25:00 -0700

    impl for count bits

--- src/num_traits/count_bits.rs
+++ src/num_traits/count_bits.rs
@@ -3,4 +3,51 @@ use super::Base;
 pub trait CountBits {
 	fn count_zeros(self) -> Self;
 	fn count_ones(self) -> Self;
+	fn leading_zeros(self) -> Self;
+	fn leading_ones(self) -> Self;
+	fn trailing_zeros(self) -> Self;
+	fn trailing_ones(self) -> Self;
+}
+
+macro_rules! impl_num_trait_count_bits {
+	{ $($num:ident)* } => {
+		$(
+			impl CountBits for $num {
+				#[inline]
+				fn count_zeros(self) -> $num {
+					$num::count_zeros(self) as _
+				}
+
+				#[inline]
+				fn count_ones(self) -> $num {
+					$num::count_ones(self) as _
+				}
+
+				#[inline]
+				fn leading_zeros(self) -> $num {
+					$num::leading_zeros(self) as _
+				}
+
+				#[inline]
+				fn leading_ones(self) -> $num {
+					$num::leading_ones(self) as _
+				}
+
+				#[inline]
+				fn trailing_zeros(self) -> $num {
+					$num::trailing_zeros(self) as _
+				}
+
+				#[inline]
+				fn trailing_ones(self) -> $num {
+					$num::trailing_ones(self) as _
+				}
+			}
+		)*
+	}
+}
+
+impl_num_trait_count_bits! {
+	u8 u16 u32 u64 u128 usize
+	i8 i16 i32 i64 i128 isize
 }
