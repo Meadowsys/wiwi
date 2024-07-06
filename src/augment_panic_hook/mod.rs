2ce89b44a6f992bf5f1bd125f09c821730bda3b3
Meadowsys <blazeykirin@gmail.com>
2024-07-05 21:28:40 -0700

    combine these back into one
    with this there's only one dyn function involved, previusly there was two (user passed in `hook`, and the inner closure type which is always it's own dyn closure)

--- src/augment_panic_hook/mod.rs
+++ src/augment_panic_hook/mod.rs
@@ -2,13 +2,9 @@ use std::panic::{ self, PanicInfo };
 
 #[inline]
 pub fn augment_panic_hook(hook: impl Fn(&PanicInfo<'_>) + Send + Sync + 'static) {
-	augment_panic_hook_inner(Box::new(hook))
-}
-
-fn augment_panic_hook_inner(hook: Box<dyn Fn(&PanicInfo<'_>) + Send + Sync + 'static>) {
 	let old = panic::take_hook();
 	panic::set_hook(Box::new(move |panic_info| {
 		hook(panic_info);
-		old(panic_info)
+		old(panic_info);
 	}))
 }
