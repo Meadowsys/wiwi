481268111e70a69d5af3e426c05acbfde787ccfa
Meadowsys <blazeykirin@gmail.com>
2024-07-17 18:44:07 -0700

    this can be, simplified a bit

--- src/unicode/char.rs
+++ src/unicode/char.rs
@@ -14,22 +14,17 @@ impl Char {
 
 impl fmt::Display for Char {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		use _internal::CodepointUtf8::*;
-		use std::str::from_utf8_unchecked;
-
 		// SAFETY: Char is always valid unicode codepoint
-		match unsafe { _internal::codepoint_to_utf8_unchecked(self.inner) } {
-			// SAFETY: Char is always valid unicode codepoint, so its encoded output is valid UTF-8
-			One { values } => { f.write_str(unsafe { from_utf8_unchecked(&values) }) }
+		let cp = unsafe { _internal::codepoint_to_utf8_unchecked(self.inner) };
 
-			// SAFETY: Char is always valid unicode codepoint, so its encoded output is valid UTF-8
-			Two { values } => { f.write_str(unsafe { from_utf8_unchecked(&values) }) }
+		let utf8: &[u8] = match &cp {
+			_internal::CodepointUtf8::One { values } => { values }
+			_internal::CodepointUtf8::Two { values } => { values }
+			_internal::CodepointUtf8::Three { values } => { values }
+			_internal::CodepointUtf8::Four { values } => { values }
+		};
 
-			// SAFETY: Char is always valid unicode codepoint, so its encoded output is valid UTF-8
-			Three { values } => { f.write_str(unsafe { from_utf8_unchecked(&values) }) }
-
-			// SAFETY: Char is always valid unicode codepoint, so its encoded output is valid UTF-8
-			Four { values } => { f.write_str(unsafe { from_utf8_unchecked(&values) }) }
-		}
+		// SAFETY: Char is always valid unicode codepoint, so its encoded output is valid UTF-8
+		f.write_str(unsafe { std::str::from_utf8_unchecked(utf8) })
 	}
 }
