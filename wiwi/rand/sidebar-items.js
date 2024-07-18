463d89b51813246008ac0a96f5767abb208407f9
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2024-07-18 23:50:28 +0000

    (automated) deploy from commit d48b45b9c4e656bb92006560f6a9c823b5fec236

--- wiwi/rand/sidebar-items.js
+++ wiwi/rand/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ThreadLocalChaCha12Rng","ThreadLocalChaCha20Rng","ThreadLocalChaCha8Rng"]};
+window.SIDEBAR_ITEMS = {"struct":["ThreadLocalChaCha12Rng","ThreadLocalChaCha20Rng","ThreadLocalChaCha8Rng"],"trait":["Randomisable"]};
