b807eba39cd9556874d55c05772d8d619c9c866b
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2024-08-13 10:14:07 +0000

    (automated) deploy from commit 35825fc96790ab866655035ae2006719aa8e012a

--- wiwi/hex/sidebar-items.js
+++ wiwi/hex/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["TABLE_ENCODER_LEN","TABLE_ENCODER_LOWER","TABLE_ENCODER_UPPER"],"enum":["DecodeError"],"fn":["decode_hex","encode_hex","encode_hex_upper"]};
+window.SIDEBAR_ITEMS = {"constant":["TABLE_ENCODER_LEN"],"enum":["DecodeError"],"fn":["decode_hex","encode_hex","encode_hex_upper"],"static":["TABLE_ENCODER_LOWER","TABLE_ENCODER_UPPER"]};
