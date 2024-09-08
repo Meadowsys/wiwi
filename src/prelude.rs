d8c1da2bf10f3bce0d4fefaedfb41171dff0647c
Meadow Liu <meadowsys@kiwin.gay>
2024-09-07 23:40:47 -0700

    format?

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,5 +1,16 @@
 //! Prelude module, exporting commonly-used items
 
-pub use crate::option::{ Option, Option::Some, Option::None };
-pub use crate::phantom::{ PhantomData, PhantomPinned };
-pub use crate::result::{ Result, Result::Ok, Result::Err };
+pub use crate::option::{
+	Option,
+	Option::Some,
+	Option::None
+};
+pub use crate::phantom::{
+	PhantomData,
+	PhantomPinned
+};
+pub use crate::result::{
+	Result,
+	Result::Ok,
+	Result::Err
+};
