0ec9d6112dad9113833844ff498ec5a8cc82ae2e
Meadow Liu <meadowsys@kiwin.gay>
2024-09-07 21:43:11 -0700

    remove gen-features script and update the others..

--- scripts/src/bin/generate-hex-table-decoder.rs
+++ scripts/src/bin/generate-hex-table-decoder.rs
@@ -1,36 +1,36 @@
 fn main() {
-	// we'll encode both upper and lower into the same table
-	use wiwi::hex::{ TABLE_ENCODER_LOWER, TABLE_ENCODER_UPPER };
+	// // we'll encode both upper and lower into the same table
+	// use wiwi::hex::{ TABLE_ENCODER_LOWER, TABLE_ENCODER_UPPER };
 
-	let mut string = String::new();
+	// let mut string = String::new();
 
-	string.push_str("pub const TABLE_DECODER: [Option<u8>; 256] = [");
-	for i in 0..=u8::MAX {
-		if i & 0b1111 == 0 { string.push_str("\n\t") }
+	// string.push_str("pub const TABLE_DECODER: [Option<u8>; 256] = [");
+	// for i in 0..=u8::MAX {
+	// 	if i & 0b1111 == 0 { string.push_str("\n\t") }
 
-		let char_lower = TABLE_ENCODER_LOWER.iter()
-			.enumerate()
-			.find(|(_, b)| **b == i)
-			.map(|(i, _)| i);
-		let char_upper = TABLE_ENCODER_UPPER.iter()
-			.enumerate()
-			.find(|(_, b)| **b == i)
-			.map(|(i, _)| i);
+	// 	let char_lower = TABLE_ENCODER_LOWER.iter()
+	// 		.enumerate()
+	// 		.find(|(_, b)| **b == i)
+	// 		.map(|(i, _)| i);
+	// 	let char_upper = TABLE_ENCODER_UPPER.iter()
+	// 		.enumerate()
+	// 		.find(|(_, b)| **b == i)
+	// 		.map(|(i, _)| i);
 
-		if let Some(num) = char_lower {
-			string.push_str(&format!("Some(0x{num:02x}), "));
-		} else if let Some(num) = char_upper {
-			string.push_str(&format!("Some(0x{num:02x}), "));
-		} else {
-			string.push_str("None,       ");
-		}
-	}
+	// 	if let Some(num) = char_lower {
+	// 		string.push_str(&format!("Some(0x{num:02x}), "));
+	// 	} else if let Some(num) = char_upper {
+	// 		string.push_str(&format!("Some(0x{num:02x}), "));
+	// 	} else {
+	// 		string.push_str("None,       ");
+	// 	}
+	// }
 
-	for _ in 0..",       ".len() {
-		string.pop();
-	}
+	// for _ in 0..",       ".len() {
+	// 	string.pop();
+	// }
 
-	string.push_str("\n];");
+	// string.push_str("\n];");
 
-	println!("{string}");
+	// println!("{string}");
 }
--- scripts/src/bin/generate-z85-table-decoder.rs
+++ scripts/src/bin/generate-z85-table-decoder.rs
@@ -1,29 +1,29 @@
 fn main() {
-	use wiwi::z85::TABLE_ENCODER;
+	// use wiwi::z85::TABLE_ENCODER;
 
-	let mut string = String::new();
+	// let mut string = String::new();
 
-	string.push_str("pub const TABLE_DECODER: [Option<u8>; 256] = [");
-	for i in 0..=u8::MAX {
-		if i & 0b1111 == 0 { string.push_str("\n\t") }
+	// string.push_str("pub const TABLE_DECODER: [Option<u8>; 256] = [");
+	// for i in 0..=u8::MAX {
+	// 	if i & 0b1111 == 0 { string.push_str("\n\t") }
 
-		let char = TABLE_ENCODER.iter()
-			.enumerate()
-			.find(|(_, b)| **b == i)
-			.map(|(i, _)| i);
+	// 	let char = TABLE_ENCODER.iter()
+	// 		.enumerate()
+	// 		.find(|(_, b)| **b == i)
+	// 		.map(|(i, _)| i);
 
-		if let Some(num) = char {
-			string.push_str(&format!("Some(0x{num:02x}), "));
-		} else {
-			string.push_str("None,       ");
-		}
-	}
+	// 	if let Some(num) = char {
+	// 		string.push_str(&format!("Some(0x{num:02x}), "));
+	// 	} else {
+	// 		string.push_str("None,       ");
+	// 	}
+	// }
 
-	for _ in 0..",       ".len() {
-		string.pop();
-	}
+	// for _ in 0..",       ".len() {
+	// 	string.pop();
+	// }
 
-	string.push_str("\n];");
+	// string.push_str("\n];");
 
-	println!("{string}");
+	// println!("{string}");
 }
