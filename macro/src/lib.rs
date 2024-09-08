345d623fd5842f5d7564d33a5dbce28124296655
Meadow Liu <meadowsys@kiwin.gay>
2024-09-07 21:49:56 -0700

    update the macro crate also

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -7,34 +7,34 @@
 	unused_variables
 )]
 
-#[cfg(feature = "memory-usage")]
-mod memory_usage;
+// #[cfg(feature = "memory-usage")]
+// mod memory_usage;
 
-#[cfg(feature = "memory-usage")]
-#[proc_macro_derive(MemoryUsage, attributes(wiwi))]
-pub fn memory_usage(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	memory_usage::macro_impl(input)
-}
+// #[cfg(feature = "memory-usage")]
+// #[proc_macro_derive(MemoryUsage, attributes(wiwi))]
+// pub fn memory_usage(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+// 	memory_usage::macro_impl(input)
+// }
 
-#[cfg(feature = "int")]
-mod int;
+// #[cfg(feature = "int")]
+// mod int;
 
-/// Internal macro only, do not use
-#[cfg(feature = "int")]
-#[doc(hidden)]
-#[proc_macro]
-pub fn define_int(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	int::macro_impl(input)
-}
+// /// Internal macro only, do not use
+// #[cfg(feature = "int")]
+// #[doc(hidden)]
+// #[proc_macro]
+// pub fn define_int(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+// 	int::macro_impl(input)
+// }
 
-#[cfg(feature = "serialiser-binary")]
-mod serialiser_binary;
+// #[cfg(feature = "serialiser-binary")]
+// mod serialiser_binary;
 
-#[cfg(feature = "serialiser-binary")]
-#[proc_macro_derive(Serialise, attributes(wiwi))]
-pub fn serialise_binary(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
-	serialiser_binary::macro_impl(input)
-}
+// #[cfg(feature = "serialiser-binary")]
+// #[proc_macro_derive(Serialise, attributes(wiwi))]
+// pub fn serialise_binary(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+// 	serialiser_binary::macro_impl(input)
+// }
 
-#[cfg(feature = "unicode")]
-mod unicode;
+// #[cfg(feature = "unicode")]
+// mod unicode;
