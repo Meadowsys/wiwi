05390a514284bdddd0c22111132a2359f8089aa1
Meadow Liu <meadowsys@kiwin.gay>
2024-11-02 00:06:43 -0700

    some more work on the string thing

--- src/prelude_std.rs
+++ src/prelude_std.rs
@@ -37,6 +37,7 @@ pub use std::{
 	array,
 	env,
 	ptr,
+	slice,
 	str
 };
 
--- src/string.rs
+++ src/string.rs
@@ -10,6 +10,7 @@ const CAP_MARKER_U8: u8 = !(u8::MAX >> 1);
 const _: () = assert!(size_of::<StringInlineable>() == STRING_SIZE_BYTES);
 const _: () = assert!(size_of::<StringInline>() == STRING_SIZE_BYTES);
 const _: () = assert!(size_of::<StringHeap>() == STRING_SIZE_BYTES);
+const _: () = assert!(MAX_INLINE_LEN > 0);
 
 pub union StringInlineable {
 	inline: ManuallyDrop<StringInline>,
@@ -19,11 +20,8 @@ pub union StringInlineable {
 impl StringInlineable {
 	#[inline]
 	pub const fn new() -> Self {
-		let inline = ManuallyDrop::new(StringInline {
-			len: 0,
-			rest: MaybeUninit::uninit()
-		});
-		Self { inline }
+		let inline = StringInline::new();
+		Self { inline: ManuallyDrop::new(inline) }
 	}
 }
 
@@ -42,6 +40,16 @@ impl StringInlineable {
 	pub fn is_empty(&self) -> bool {
 		self.len() == 0
 	}
+
+	#[inline]
+	pub fn as_str(&self) -> &str {
+		self.do_thing(|s| s.as_str(), |s| s.as_str())
+	}
+
+	#[inline]
+	pub fn as_str_mut(&mut self) -> &mut str {
+		self.do_thing_mut(|s| s.as_str_mut(), |s| s.as_str_mut())
+	}
 }
 
 impl StringInlineable {
@@ -55,10 +63,10 @@ impl StringInlineable {
 	}
 
 	#[inline(always)]
-	fn do_thing<T, FInline, FHeap>(&self, f_inline: FInline, f_heap: FHeap) -> T
+	fn do_thing<'h, T, FInline, FHeap>(&'h self, f_inline: FInline, f_heap: FHeap) -> T
 	where
-		FInline: FnOnce(&StringInline) -> T,
-		FHeap: FnOnce(&StringHeap) -> T
+		FInline: FnOnce(&'h StringInline) -> T,
+		FHeap: FnOnce(&'h StringHeap) -> T
 	{
 		match self.is_inline() {
 			// SAFETY: we just checked `self.is_inline()`
@@ -69,10 +77,10 @@ impl StringInlineable {
 	}
 
 	#[inline(always)]
-	fn do_thing_mut<T, FInline, FHeap>(&mut self, f_inline: FInline, f_heap: FHeap) -> T
+	fn do_thing_mut<'h, T, FInline, FHeap>(&'h mut self, f_inline: FInline, f_heap: FHeap) -> T
 	where
-		FInline: FnOnce(&mut StringInline) -> T,
-		FHeap: FnOnce(&mut StringHeap) -> T
+		FInline: FnOnce(&'h mut StringInline) -> T,
+		FHeap: FnOnce(&'h mut StringHeap) -> T
 	{
 		match self.is_inline() {
 			// SAFETY: we just checked `self.is_inline()`
@@ -90,40 +98,29 @@ impl Default for StringInlineable {
 	}
 }
 
+impl Deref for StringInlineable {
+	type Target = str;
+
+	#[inline]
+	fn deref(&self) -> &str {
+		self.as_str()
+	}
+}
+
 impl From<&str> for StringInlineable {
-	fn from(value: &str) -> Self {
-		match value.len() <= MAX_INLINE_LEN {
+	#[inline]
+	fn from(s: &str) -> Self {
+		match s.len() <= MAX_INLINE_LEN {
 			true => {
-				// TODO: move all this code into like, from_str_unsafe
-				// function in StringInline
-				let mut inline = ManuallyDrop::new(StringInline {
-					len: 0,
-					rest: MaybeUninit::uninit()
-				});
-
-				// SAFETY:
-				// - ptr obtained from `value` is valid, and for `value.len()` reads
-				// - ptr obtained from `inline.rest` is valid, and we checked
-				//   `value.len()` to be lte its length (`MAX_INLINE_LEN`)
-				// - ptrs obtained from aligned sources
-				// - passed in reference to memory in `value` cannot overlap with
-				//   function local stack memory in `inline`
-				unsafe {
-					ptr::copy_nonoverlapping(
-						value.as_ptr(),
-						inline.rest.as_mut_ptr().cast::<u8>(),
-						value.len()
-					)
-				}
-
-				// we just initialised `value.len()` amount of
-				// memory with that `copy` call above
-				inline.len = value.len().into_u8_lossy();
-
-				Self { inline }
+				// SAFETY: just checked `s.len() <= MAX_INLINE_LEN`
+				let inline = unsafe { StringInline::from_str_unchecked(s) };
+				Self { inline: ManuallyDrop::new(inline) }
 			}
 			false => {
-				todo!()
+				// SAFETY: just checked `s.len() > MAX_INLINE_LEN`
+				// (which is also not zero)
+				let heap = unsafe { StringHeap::from_str_unchecked(s) };
+				Self { heap: ManuallyDrop::new(heap) }
 			}
 		}
 	}
@@ -138,6 +135,47 @@ struct StringInline {
 
 impl StringInline {
 	#[inline]
+	const fn new() -> Self {
+		Self { len: 0, rest: MaybeUninit::uninit() }
+	}
+
+	/// # Safety
+	///
+	/// The passed in `str` must have length less than or equal to [`MAX_INLINE_LEN`].
+	#[inline]
+	unsafe fn from_str_unchecked(s: &str) -> Self {
+		debug_assert!(s.len() <= MAX_INLINE_LEN);
+
+		let mut inline = Self {
+			len: 0,
+			rest: MaybeUninit::uninit()
+		};
+
+		// SAFETY:
+		// - ptr obtained from `value` is valid, and for `s.len()` reads
+		// - ptr obtained from `inline.rest` is valid
+		// - caller promises `s.len()` is lte `MAX_INLINE_LEN`
+		// - ptrs obtained from aligned sources
+		// - reference to memory outside local stack memory in `value`
+		//   cannot overlap with local stack memory in `inline`
+		unsafe {
+			ptr::copy_nonoverlapping(
+				s.as_ptr(),
+				inline.rest.as_mut_ptr().cast::<u8>(),
+				s.len()
+			)
+		}
+
+		// we just initialised `s.len()` amount of
+		// memory with that `copy` call above
+		inline.len = s.len().into_u8_lossy();
+
+		inline
+	}
+}
+
+impl StringInline {
+	#[inline]
 	fn len(&self) -> usize {
 		usize::from_u8(self.len)
 	}
@@ -145,6 +183,34 @@ impl StringInline {
 	#[inline]
 	fn capacity(&self) -> usize {
 		MAX_INLINE_LEN
+	}
+
+	// TODO: replace as with .into_usize() when that's a thing
+	#[expect(clippy::as_conversions)]
+	#[inline]
+	fn as_str(&self) -> &str {
+		let ptr = self.rest.as_ptr().cast::<u8>();
+		let len = self.len as usize;
+
+		// SAFETY: relying on invariant that `self.rest` must have
+		// at least `self.len` elements initialised
+		let slice = unsafe { slice::from_raw_parts(ptr, len) };
+		// SAFETY: relying on invariant that `self` contains valid utf-8
+		unsafe { str::from_utf8_unchecked(slice) }
+	}
+
+	// TODO: replace as with .into_usize() when that's a thing
+	#[expect(clippy::as_conversions)]
+	#[inline]
+	fn as_str_mut(&mut self) -> &mut str {
+		let ptr = self.rest.as_ptr().cast::<u8>();
+		let len = self.len as usize;
+
+		// SAFETY: relying on invariant that `self.rest` must have
+		// at least `self.len` elements initialised
+		let slice = unsafe { slice::from_raw_parts_mut(ptr.cast_mut(), len) };
+		// SAFETY: relying on invariant that `self` contains valid utf-8
+		unsafe { str::from_utf8_unchecked_mut(slice) }
 	}
 }
 
@@ -160,6 +226,49 @@ struct StringHeap {
 }
 
 impl StringHeap {
+	/// # Safety
+	///
+	/// The passed in `str` must have length greater than zero. (The passed in
+	/// `str` _should_ have greater than `MAX_INLINE_LEN` len, which is larger
+	/// than zero, and `StringInlineable` already ensures this)
+	unsafe fn from_str_unchecked(s: &str) -> Self {
+		let layout = alloc_mod::Layout::for_value(s);
+		// SAFETY: layout is nonzero (caller promises `s` is not zero length)
+		let ptr = unsafe { alloc(layout) };
+
+		let mut heap = Self {
+			cap_be_and_marker: 0,
+			len: 0,
+			ptr
+		};
+		// SAFETY:
+		// - we just allocated the ptr inside with this layout
+		// - existing `&str` cannot have memory larger than `isize::MAX`
+		unsafe { heap.set_capacity(layout.size()) }
+
+		// SAFETY:
+		// - ptr obtained from `value` is valid, and for `s.len()` reads
+		// - we just allocated ptr in `heap.ptr` for `s.len()` bytes
+		// - ptrs obtained from aligned sources
+		// - reference to existing memory in `value` cannot overlap with
+		//   memory we just allocated
+		unsafe {
+			ptr::copy_nonoverlapping(
+				s.as_ptr(),
+				heap.ptr.cast_mut(),
+				s.len()
+			)
+		}
+
+		// we just initialised `s.len()` amount of
+		// memory with that `copy` call above
+		heap.len = s.len();
+
+		heap
+	}
+}
+
+impl StringHeap {
 	#[inline]
 	fn len(&self) -> usize {
 		self.len
@@ -168,5 +277,36 @@ impl StringHeap {
 	#[inline]
 	fn capacity(&self) -> usize {
 		usize::from_be(self.cap_be_and_marker ^ CAP_MARKER_BE)
+	}
+
+	/// Helper for setting capacity (since it's stored in a... nonstandard way)
+	///
+	/// # Safety
+	///
+	/// - Capacity in `self` must actually be `capacity`
+	/// - `capacity` must be less than or equal to `isize::MAX`. This is
+	///   required by rust's allocation APIs, as well as needed for the heap marker
+	///   to be set properlu
+	#[inline]
+	unsafe fn set_capacity(&mut self, capacity: usize) {
+		self.cap_be_and_marker = capacity.to_be() ^ CAP_MARKER_BE
+	}
+
+	#[inline]
+	fn as_str(&self) -> &str {
+		// SAFETY: relying on invariant that `self.rest` must have
+		// at least `self.len` elements initialised
+		let slice = unsafe { slice::from_raw_parts(self.ptr, self.len) };
+		// SAFETY: relying on invariant that `self` contains valid utf-8
+		unsafe { str::from_utf8_unchecked(slice) }
+	}
+
+	#[inline]
+	fn as_str_mut(&mut self) -> &mut str {
+		// SAFETY: relying on invariant that `self.rest` must have
+		// at least `self.len` elements initialised
+		let slice = unsafe { slice::from_raw_parts_mut(self.ptr.cast_mut(), self.len) };
+		// SAFETY: relying on invariant that `self` contains valid utf-8
+		unsafe { str::from_utf8_unchecked_mut(slice) }
 	}
 }
