993964ac5e49088b0d70e2a4dbcd834c96c3d71f
Meadow Liu <meadowsys@kiwin.gay>
2024-11-09 03:36:33 -0800

    move rc str to its own module and impl some more boilerplate things

--- src/rc.rs
+++ src/rc.rs
@@ -1,10 +1,19 @@
 use crate::prelude_std::*;
 
 pub use self::inner::{ Counter, ThreadCounter, AtomicCounter };
+pub use self::str::{
+	RcStr,
+	RcStrWeak,
+	RcStrThread,
+	RcStrThreadWeak,
+	RcStrAtomic,
+	RcStrAtomicWeak
+};
 
 /// This module encapsulates and provides low level manipulation APIs on
 /// a raw and opaque inner reference counting pointer
 mod inner;
+mod str;
 
 /// Reference counted thin pointer, that can hold one sized
 /// value and one (dynamically sized) slice
@@ -247,51 +256,3 @@ unsafe impl<C: Send + Counter, V: Send, S: Send> Send for RcWeak<C, V, S> {}
 unsafe impl<C: Sync + Counter, V: Sync, S: Sync> Sync for Rc<C, V, S> {}
 // SAFETY: same as above
 unsafe impl<C: Sync + Counter, V: Sync, S: Sync> Sync for RcWeak<C, V, S> {}
-
-#[repr(transparent)]
-pub struct RcStr<C: Counter, M = ()> {
-	inner: Rc<C, M, u8>
-}
-
-#[repr(transparent)]
-pub struct RcStrWeak<C: Counter, M = ()> {
-	inner: RcWeak<C, M, u8>
-}
-
-/// Single threaded reference counting thin pointer to a [`str`],
-/// optionally carrying arbitrary additional metadata
-pub type RcStrThread<M = ()> = RcStr<ThreadCounter, M>;
-
-/// Weak pointer to a single threaded reference counted thin pointer [`RcStrThread`]
-pub type RcStrThreadWeak<M = ()> = RcStrWeak<ThreadCounter, M>;
-
-/// Atomically counted reference counting thin pointer to a [`str`],
-/// optionally carrying arbitrary additional metadata
-pub type RcStrAtomic<M = ()> = RcStr<AtomicCounter, M>;
-
-/// Weak pointer to an atomically counted reference counted thin pointer [`RcStrAtomic`]
-pub type RcStrAtomicWeak<M = ()> = RcStrWeak<AtomicCounter, M>;
-
-impl<C: Counter> RcStr<C> {
-	#[inline]
-	pub fn new(s: &str) -> Self {
-		Self { inner: Rc::from_slice_copy(s.as_bytes()) }
-	}
-}
-
-impl<C: Counter, M> RcStr<C, M> {
-	#[inline]
-	pub fn with_metadata(s: &str, metadata: M) -> Self {
-		Self { inner: Rc::from_value_and_slice_copy(metadata, s.as_bytes()) }
-	}
-}
-
-impl<C: Counter, M> Deref for RcStr<C, M> {
-	type Target = str;
-
-	#[inline]
-	fn deref(&self) -> &str {
-		// SAFETY: `RcStr` has variant of containing valid utf-8
-		unsafe { str::from_utf8_unchecked(self.inner.as_slice_ref()) }
-	}
-}
---
+++ src/rc/str.rs
@@ -0,0 +1,98 @@
+use crate::prelude_std::*;
+use super::{ Rc, RcWeak, Counter, ThreadCounter, AtomicCounter };
+
+#[repr(transparent)]
+pub struct RcStr<C: Counter, M = ()> {
+	inner: Rc<C, M, u8>
+}
+
+#[repr(transparent)]
+pub struct RcStrWeak<C: Counter, M = ()> {
+	inner: RcWeak<C, M, u8>
+}
+
+/// Single threaded reference counting thin pointer to a [`str`],
+/// optionally carrying arbitrary additional metadata
+pub type RcStrThread<M = ()> = RcStr<ThreadCounter, M>;
+
+/// Weak pointer to a single threaded reference counted thin pointer [`RcStrThread`]
+pub type RcStrThreadWeak<M = ()> = RcStrWeak<ThreadCounter, M>;
+
+/// Atomically counted reference counting thin pointer to a [`str`],
+/// optionally carrying arbitrary additional metadata
+pub type RcStrAtomic<M = ()> = RcStr<AtomicCounter, M>;
+
+/// Weak pointer to an atomically counted reference counted thin pointer [`RcStrAtomic`]
+pub type RcStrAtomicWeak<M = ()> = RcStrWeak<AtomicCounter, M>;
+
+impl<C: Counter> RcStr<C> {
+	#[inline]
+	pub fn new(s: &str) -> Self {
+		Self { inner: Rc::from_slice_copy(s.as_bytes()) }
+	}
+}
+
+impl<C: Counter, M> RcStr<C, M> {
+	#[inline]
+	pub fn with_metadata(s: &str, metadata: M) -> Self {
+		Self { inner: Rc::from_value_and_slice_copy(metadata, s.as_bytes()) }
+	}
+}
+
+impl<C: Counter, M> RcStr<C, M> {
+	#[inline]
+	pub fn strong_count(&self) -> usize {
+		self.inner.strong_count()
+	}
+
+	#[inline]
+	pub fn weak_count(&self) -> usize {
+		self.inner.weak_count()
+	}
+
+	#[inline]
+	pub fn downgrade(&self) -> RcStrWeak<C, M> {
+		RcStrWeak { inner: self.inner.downgrade() }
+	}
+}
+
+impl<C: Counter, M> Clone for RcStr<C, M> {
+	#[inline]
+	fn clone(&self) -> Self {
+		Self { inner: self.inner.clone() }
+	}
+}
+
+impl<C: Counter, M> Deref for RcStr<C, M> {
+	type Target = str;
+
+	#[inline]
+	fn deref(&self) -> &str {
+		// SAFETY: `RcStr` has variant of containing valid utf-8
+		unsafe { str::from_utf8_unchecked(self.inner.as_slice_ref()) }
+	}
+}
+
+impl<C: Counter, M> RcStrWeak<C, M> {
+	#[inline]
+	pub fn strong_count(&self) -> usize {
+		self.inner.strong_count()
+	}
+
+	#[inline]
+	pub fn weak_count(&self) -> usize {
+		self.inner.weak_count()
+	}
+
+	#[inline]
+	pub fn upgrade(&self) -> Option<RcStr<C, M>> {
+		self.inner.upgrade().map(|inner| RcStr { inner })
+	}
+}
+
+impl<C: Counter, M> Clone for RcStrWeak<C, M> {
+	#[inline]
+	fn clone(&self) -> Self {
+		Self { inner: self.inner.clone() }
+	}
+}
