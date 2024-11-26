838f3db7d0bd599773cdaee71ee91c905125648c
Meadow Liu <meadowsys@kiwin.gay>
2024-11-26 04:27:03 -0800

    smol trait is smol

--- src/builder.rs
+++ src/builder.rs
@@ -1,5 +1,8 @@
 extern crate wiwiwiwiwi;
 
+use crate::prelude_std::*;
+use self::private::AcceptDefaultSealed;
+
 pub use wiwiwiwiwi::builder;
 
 pub struct Uninit {
@@ -9,6 +12,37 @@ pub struct Uninit {
 pub struct Init {
 	__private: ()
 }
+
+/// Trait for required items that can take [`None`] for convenience, because
+/// they implement [`Default`]
+pub trait AcceptDefault<T>
+where
+	Self: AcceptDefaultSealed<T>
+{
+	fn unwrap_or_default(self) -> T;
+}
+
+impl<T> AcceptDefault<T> for T {
+	#[inline]
+	fn unwrap_or_default(self) -> T {
+		self
+	}
+}
+
+impl<T> AcceptDefaultSealed<T> for T {}
+
+impl<T> AcceptDefault<T> for Option<T>
+where
+	T: Default
+{
+	#[inline]
+	fn unwrap_or_default(self) -> T {
+		self.unwrap_or_default()
+	}
+}
+
+impl<T> AcceptDefaultSealed<T> for Option<T> {}
+
 
 /// More rudimentary builder generator helper
 // I think we'd need proc macro to support generic structs properly
@@ -49,3 +83,9 @@ pub use generate_builder;
 // 		1 field: crate::prelude_std::Vec<u8>
 // 	}
 // }
+
+/// notouchie
+mod private {
+	/// notouchie
+	pub trait AcceptDefaultSealed<T> {}
+}
