32983661f8b5342df0add9e0dd30d6ef4f376e8c
Meadow Liu <meadowsys@kiwin.gay>
2024-11-25 17:30:28 -0800

    builderstuff?
    I should just, go push through write proc macro version lol

--- src/builder.rs
+++ src/builder.rs
@@ -5,3 +5,43 @@ pub struct Uninit {
 pub struct Init {
 	__private: ()
 }
+
+/// More rudimentary builder generator helper
+// I think we'd need proc macro to support generic structs properly
+// (and to get rid of incrementer param)
+#[macro_export]
+macro_rules! generate_builder {
+	{
+		#[builder = $builder:ident]
+		$vis:vis struct $struct_name:ident {
+			$($incrementer:literal $field:ident: $type:ty),*
+		}
+	} => {
+		$vis struct $struct_name {
+			$($field: $type),*
+		}
+
+		#[allow(
+			non_camel_case_types,
+			clippy::allow_attributes,
+			reason = "macro impl"
+		)]
+		#[repr(transparent)]
+		$vis struct $builder<$($field = $crate::builder::Uninit),*> {
+			inner: ($($crate::prelude_std::MaybeUninit<$type>,)*),
+			__marker: $crate::prelude_std::PhantomData<
+				// invariant? ?????
+				fn(($($field,)*)) -> ($($field,)*)
+			>
+		}
+	}
+}
+pub use generate_builder;
+
+// generate_builder! {
+// 	#[builder = TestBuilder]
+// 	pub struct Test {
+// 		0 eeie: u8,
+// 		1 field: crate::prelude_std::Vec<u8>
+// 	}
+// }
