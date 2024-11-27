3435fcc3aa79c40c2372723ce7a5bdad63e2b1bf
Meadow Liu <meadowsys@kiwin.gay>
2024-11-26 22:08:47 -0800

    yeet

--- src/builder.rs
+++ src/builder.rs
@@ -43,47 +43,6 @@ where
 
 impl<T> AcceptDefaultSealed<T> for Option<T> {}
 
-
-/// More rudimentary builder generator helper
-// I think we'd need proc macro to support generic structs properly
-// (and to get rid of incrementer param)
-#[macro_export]
-macro_rules! generate_builder {
-	{
-		#[builder = $builder:ident]
-		$vis:vis struct $struct_name:ident {
-			$($incrementer:literal $field:ident: $type:ty),*
-		}
-	} => {
-		$vis struct $struct_name {
-			$($field: $type),*
-		}
-
-		#[allow(
-			non_camel_case_types,
-			clippy::allow_attributes,
-			reason = "macro impl"
-		)]
-		#[repr(transparent)]
-		$vis struct $builder<$($field = $crate::builder::Uninit),*> {
-			inner: ($($crate::prelude_std::MaybeUninit<$type>,)*),
-			__marker: $crate::prelude_std::PhantomData<
-				// invariant? ?????
-				fn(($($field,)*)) -> ($($field,)*)
-			>
-		}
-	}
-}
-pub use generate_builder;
-
-// generate_builder! {
-// 	#[builder = TestBuilder]
-// 	pub struct Test {
-// 		0 eeie: u8,
-// 		1 field: crate::prelude_std::Vec<u8>
-// 	}
-// }
-
 /// notouchie
 mod private {
 	/// notouchie
