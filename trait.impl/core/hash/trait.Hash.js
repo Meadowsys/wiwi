5553798b07c77f998d8752a2c19b8d23df2d5bbe
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2024-11-30 05:05:28 +0000

    (automated) deploy from commit 6c105542cdce5154f10eff6b1ed903712ff4a3c5

--- trait.impl/core/hash/trait.Hash.js
+++ trait.impl/core/hash/trait.Hash.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, M&gt; <a class=\"trait\" href=\"wiwi/prelude_std/trait.Hash.html\" title=\"trait wiwi::prelude_std::Hash\">Hash</a> for <a class=\"struct\" href=\"wiwi/nominal/struct.Nominal.html\" title=\"struct wiwi::nominal::Nominal\">Nominal</a>&lt;T, M&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"wiwi/prelude_std/trait.Hash.html\" title=\"trait wiwi::prelude_std::Hash\">Hash</a>,</div>"]]]]);
+    var implementors = Object.fromEntries([["wiwi",[["impl&lt;T, M&gt; <a class=\"trait\" href=\"wiwi/prelude/trait.Hash.html\" title=\"trait wiwi::prelude::Hash\">Hash</a> for <a class=\"struct\" href=\"wiwi/nominal/struct.Nominal.html\" title=\"struct wiwi::nominal::Nominal\">Nominal</a>&lt;T, M&gt;<div class=\"where\">where\n    T: <a class=\"trait\" href=\"wiwi/prelude/trait.Hash.html\" title=\"trait wiwi::prelude::Hash\">Hash</a>,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[422]}
+//{"start":57,"fragment_lengths":[406]}
