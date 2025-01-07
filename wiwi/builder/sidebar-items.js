3177059da796fbda531edfd9ad2db78580e3fe9c
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2025-01-07 14:48:04 +0000

    (automated) deploy from commit 6cb503508a0ad5c572290cceb6070072217a0ce3

--- wiwi/builder/sidebar-items.js
+++ wiwi/builder/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"attr":["builder"],"struct":["Init","Uninit"],"trait":["AcceptDefault"]};
+window.SIDEBAR_ITEMS = {"attr":["builder"],"struct":["Init","Uninit"],"trait":["AcceptDefault","InitialisationStatus"]};
