d0a4f60deb67d675cd1b738ba2f38b17d74f30dc
smol <107521333+a-tiny-kirin@users.noreply.github.com>
2025-01-09 11:10:05 +0000

    (automated) deploy from commit 06721525fb8f31dafc6fe03f8bb25bb3bd6b2279

--- wiwi/mem_use/sidebar-items.js
+++ wiwi/mem_use/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"trait":["MemUse","MemUseConst","MemUseStatic"]};
+window.SIDEBAR_ITEMS = {"fn":["mem_use","mem_use_const","mem_use_const_indirect","mem_use_const_indirect_maybe","mem_use_const_inline","mem_use_const_inline_maybe","mem_use_const_maybe","mem_use_indirect","mem_use_inline"],"trait":["MemUse","MemUseConst"]};
